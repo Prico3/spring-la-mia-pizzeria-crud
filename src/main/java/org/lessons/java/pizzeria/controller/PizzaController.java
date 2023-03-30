63fc2efda221afb9e5c32b3a0f838ee54c522ef2
Prico3 <pricoco.andrea@gmail.com>
2023-03-30 17:38:50 +0200

    add search

--- src/main/java/org/lessons/java/pizzeria/controller/PizzaController.java
+++ src/main/java/org/lessons/java/pizzeria/controller/PizzaController.java
@@ -3,12 +3,14 @@ package org.lessons.java.pizzeria.controller;
 import org.lessons.java.pizzeria.model.Pizza;
 import org.lessons.java.pizzeria.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -21,9 +23,16 @@ public class PizzaController {
     private PizzaRepository pizzaRepository;
 
     @GetMapping
-    public String index(Model model) {
-        List<Pizza> pizzas = pizzaRepository.findAll();
+    public String index(Model model, @RequestParam(name = "q") Optional<String> keyword) {
+        List<Pizza> pizzas;
+        if (keyword.isEmpty()) {
+            pizzas = pizzaRepository.findAll(Sort.by("name"));
+        } else {
+            pizzas = pizzaRepository.findByNameContainingIgnoreCase(keyword.get());
+            model.addAttribute("keyword", keyword.get());
+        }
         model.addAttribute("list", pizzas);
+
         return "/pizzas/index";
     }
 
--- src/main/java/org/lessons/java/pizzeria/repository/PizzaRepository.java
+++ src/main/java/org/lessons/java/pizzeria/repository/PizzaRepository.java
@@ -3,5 +3,10 @@ package org.lessons.java.pizzeria.repository;
 import org.lessons.java.pizzeria.model.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
+    public List<Pizza> findByNameContainingIgnoreCase(String name);
+
+
 }
