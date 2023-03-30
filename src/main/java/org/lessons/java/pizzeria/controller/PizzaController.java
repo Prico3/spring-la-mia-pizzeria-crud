13911db40c88569f76cef4874b8ca6a86cbfa8a1
Prico3 <pricoco.andrea@gmail.com>
2023-03-30 16:58:42 +0200

    add show method

--- src/main/java/org/lessons/java/pizzeria/controller/PizzaController.java
+++ src/main/java/org/lessons/java/pizzeria/controller/PizzaController.java
@@ -3,12 +3,16 @@ package org.lessons.java.pizzeria.controller;
 import org.lessons.java.pizzeria.model.Pizza;
 import org.lessons.java.pizzeria.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/pizzas")
@@ -21,5 +25,16 @@ public class PizzaController {
         List<Pizza> pizzas = pizzaRepository.findAll();
         model.addAttribute("list", pizzas);
         return "/pizzas/index";
+    }
+
+    @GetMapping("/{pizzaId}")
+    public String show(@PathVariable("pizzaId") Integer id, Model model) {
+        Optional<Pizza> result = pizzaRepository.findById(id);
+        if (result.isPresent()) {
+            model.addAttribute("pizza", result.get());
+            return "pizzas/show";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza with id " + id + " not found");
+        }
     }
 }
